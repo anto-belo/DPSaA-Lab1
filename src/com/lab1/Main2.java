f618b2eca96bef5751ad25a83d3bcd0f9f195ea3
anto-belo <70717945+anto-belo@users.noreply.github.com>
2020-09-07 10:17:26 +0300

    Watchers added

--- src/com/lab1/Main2.java
+++ src/com/lab1/Main2.java
@@ -8,11 +8,15 @@ public class Main2 {
         System.out.println("Every " + k-- + " child is removed");
 
         for (int n = 1; n <= 64; n++) {
+            System.out.println("------------------------");
             ChildCircle cc = new ChildCircle(n);
+            cc.printList();
             int index = 0;
             while (cc.size() != 1) {
                 index = cc.move(index, k);
+                System.out.println("Child #" + cc.node(index).e + " removed");
                 cc.delete(index);
+                cc.printList();
             }
             System.out.printf("%2d children: #" + cc.node(0).e + " is the last\n", n);
         }
